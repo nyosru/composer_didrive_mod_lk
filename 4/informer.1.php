539cc5dd4d7ca041cdd0fdb28c832a148d4e79f0
Сергей <nyos@rambler.ru>
2019-06-27 01:45:19 +0500

    no message

--- 4/informer.1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$vv['_inf']['lk'][4]['forms'] = \f\like_tpl( 'enter.reg.form', dirname(__FILE__).'/tpl.inf/', dir_serv_site.'module/'.$vv['now_inf_cfg']['cfg.level'].'/tpl.inf/' );
-
-if( !isset($_SESSION['now_user']['id']{0}) ){
-    $vv['in_body_end_js']['https://www.google.com/recaptcha/api.js'] = 1;
-    $vv['in_body_end_js']['//ulogin.ru/js/ulogin.js'] = 1;
-}
